fa75eeb0db1b802056c164151dcbb585e1a9c6aa
Giri Thomas <giri.thomas@stud.hochschule-heidelberg.de>
2021-02-04 23:45:43 +0100

    Static server
    Signed-off-by: Giri Thomas <giri.thomas@stud.hochschule-heidelberg.de>

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('./public'))
 
 app.use(cors());
 app.use(express.json());
